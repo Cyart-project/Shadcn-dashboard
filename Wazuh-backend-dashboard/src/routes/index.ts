e04e6b7b9958325e441d9f49d3eb9cbafa3d6d84
Cyart-project <cyartproject@gmail.com>
2025-06-09 04:19:38 -0400

    Update index.ts

--- Wazuh-backend-dashboard/src/routes/index.ts
+++ Wazuh-backend-dashboard/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import eventsRouter from './events.routes';
 import agentsRouter from './agents.routes';
+import webhookRouter from './webhook.routes';
 
 const router = Router();
 
 router.use('/events', eventsRouter);
 router.use('/agents', agentsRouter);
+router.use('/webhook', webhookRouter); // Add this line
 
 export default router;
